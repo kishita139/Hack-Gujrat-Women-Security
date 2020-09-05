fdac9077be54d20673bdc5f9c2b8625ea80065b6
xANARKYx <shikhars3145@gmail.com>
2020-09-05 20:55:15 +0530

    home page form connected to backend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,99 +1,124 @@
-import React, { Component} from 'react';
-import { Redirect ,Link} from "react-router-dom";
-import { PageHeader,Menu,Button,Drawer,Row,Col } from 'antd';
-import "./search.css";
-import "antd/dist/antd.css";
-import ReactMapGL from "react-map-gl";
-import Header from "./Header"
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect, Link } from 'react-router-dom';
+import { PageHeader, Menu, Button, Drawer, Row, Col } from 'antd';
+import './search.css';
+import 'antd/dist/antd.css';
+import ReactMapGL from 'react-map-gl';
+import Header from './Header';
 export default class Home extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            viewport: {
-                latitude: 21.2786567,
-                longitude: 81.8661442,
-                width: "100vw",
-                height: "100vh",
-                zoom: 10
-            },
-            token: "pk.eyJ1Ijoia3NoYXRha3NoaSIsImEiOiJja2VuOHl5b24weWFwMnJtcXF5MXR0ZDFhIn0.dUfYcwHvsc5e8AOLg0ZKJA",
-            collapsed: false,
-            signup: false,
-            from: "",
-            to: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 21.2786567,
+        longitude: 81.8661442,
+        width: '100vw',
+        height: '100vh',
+        zoom: 10,
+      },
+      token:
+        'pk.eyJ1Ijoia3NoYXRha3NoaSIsImEiOiJja2VuOHl5b24weWFwMnJtcXF5MXR0ZDFhIn0.dUfYcwHvsc5e8AOLg0ZKJA',
+      collapsed: false,
+      signup: false,
+      from: '',
+      to: '',
+    };
+  }
+  getPosition = (position) => {
+    this.setState((prevState) => ({
+      viewport: {
+        // object that we want to update
+        ...prevState.viewport, // keep all other key-value pairs
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude, // update the value of specific key
+      },
+    }));
+  };
 
-        };
-      }
-     getPosition=(position)=>{
-        this.setState(prevState => ({
-            viewport: {                   // object that we want to update
-                ...prevState.viewport,    // keep all other key-value pairs
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude     // update the value of specific key
-            }
-        }))
+  getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(this.getPosition);
+    } else {
+      console.log('error');
     }
+  };
 
-    getLocation=()=>{
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(this.getPosition);
-          } else { 
-            console.log("error");
-          }
-    }
-      
-    onSubmit = () =>{
-        console.log("submit");
-    }
+  onSubmit = async (e) => {
+    e.preventDefault();
 
-    changeHandler = e => {
-        let value = e.target.value.replace(/<[^>]*>?/gm, "");
-    
-        this.setState({
-           [e.target.name]: value 
-        });
-      };  
+    console.log(this.state.from);
+    console.log(this.state.to);
 
-    componentDidMount=()=>{
-        this.getLocation();
-    }
-      
-    render() {
-            return (
-                <div>
-                    
-                   <Header/>
-                   <center>
-                   <form onSubmit={this.onSubmit} method="POST" className="form">
-          
-                        <Row><Col xs={7}><span className="form-fields required">From </span></Col> 
-                        <Col xs={15}><input
-                            className="form-control1"
-                            type="text"
-                            name="from"
-                            value={this.state.from}
-                            onChange={this.changeHandler}
-                        /></Col></Row>
-                        <Row><Col xs={7}><span className="form-fields required">To </span></Col>
-                        <Col xs={15}><input
-                            className="form-control1"
-                            type="text"
-                            name="to"
-                            value={this.state.to}
-                            onChange={this.changeHandler}
-                        /></Col></Row>
-                        <Button type="primary" htmlType="submit">Submit</Button><br/>
-                    </form>
-                    </center>
-                    <ReactMapGL {...this.state.viewport} 
-                    mapboxApiAccessToken={this.state.token}
-                    onViewportChange={(viewport=>this.setState({viewport}))}
-                    mapStyle="mapbox://styles/kshatakshi/cken9e08331um19mt5bn8gnp6">
-                    </ReactMapGL>
-                </div>
-            )
-        }
-        
-    
+    const res = await axios.post('/api/roads/getSafestRoute', {
+      startAdd: this.state.from,
+      destAdd: this.state.to,
+    });
+
+    console.log(res.data);
+
+    // console.log('submit');
+  };
+
+  changeHandler = (e) => {
+    let value = e.target.value.replace(/<[^>]*>?/gm, '');
+
+    this.setState({
+      [e.target.name]: value,
+    });
+  };
+
+  componentDidMount = () => {
+    this.getLocation();
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <center>
+          <form onSubmit={this.onSubmit} method="POST" className="form">
+            <Row>
+              <Col xs={7}>
+                <span className="form-fields required">From </span>
+              </Col>
+              <Col xs={15}>
+                <input
+                  className="form-control1"
+                  type="text"
+                  name="from"
+                  value={this.state.from}
+                  onChange={this.changeHandler}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={7}>
+                <span className="form-fields required">To </span>
+              </Col>
+              <Col xs={15}>
+                <input
+                  className="form-control1"
+                  type="text"
+                  name="to"
+                  value={this.state.to}
+                  onChange={this.changeHandler}
+                />
+              </Col>
+            </Row>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <br />
+          </form>
+        </center>
+        <ReactMapGL
+          {...this.state.viewport}
+          mapboxApiAccessToken={this.state.token}
+          onViewportChange={(viewport) => this.setState({ viewport })}
+          mapStyle="mapbox://styles/kshatakshi/cken9e08331um19mt5bn8gnp6"
+        ></ReactMapGL>
+      </div>
+    );
+  }
 }
--- server/controller/roadController.js
+++ server/controller/roadController.js
@@ -13,21 +13,71 @@ const getCoordinates = async (address) => {
   return location;
 };
 
+// long,lat
+const getRoutes = async (start, end) => {
+  try {
+    const directionsRes = await axios.get(
+      `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&alternatives=true&geometries=geojson&access_token=${process.env.MAPBOX_TOKEN}`
+    );
+    // console.log(directionsRes.data.routes);
+    // console.log(directionsRes.data.routes[0].geometry.coordinates.length);
+    return directionsRes.data.routes;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getRoads = async (routes) => {
+  const roadsInRoutes = [];
+  routes.forEach((route) => {
+    const road = [];
+    const steps = route.legs[0].steps;
+
+    steps.forEach((step) => {
+      const found = road.some((ele) => ele.name === step.name);
+      if (step.name && step.name !== '' && !found)
+        road.push({ name: step.name, loc: step.maneuver.location });
+      //   console.log(step.name);
+    });
+
+    roadsInRoutes.push(road);
+  });
+
+  return roadsInRoutes;
+};
+
 exports.getSafestRoutes = async (req, res, next) => {
+  // convert addresses to coords
   const startAdd = req.body.startAdd;
   const destAdd = req.body.destAdd;
 
-  console.log(startAdd, destAdd);
+  // console.log(startAdd, destAdd);
 
   const startLoc = await getCoordinates(startAdd);
   const destLoc = await getCoordinates(destAdd);
 
-  console.log(startLoc, destLoc);
+  // console.log(startLoc, destLoc);
+
+  // get all the routes from start to dest
+  const routes = await getRoutes(
+    [startLoc.lng, startLoc.lat],
+    [destLoc.lng, destLoc.lat]
+  );
+
+  // console.log(routes);
+
+  // get roads in each route
+  const roadsInRoutes = await getRoads(routes);
+  console.dir(roadsInRoutes, { depth: null });
+
+  // filter roads not present in DB
+  // const filteredRoads =
 
   res.status(200).json({
     status: 'success',
     data: {
       routes: dummyRoutes,
+      // routes: routes,
     },
   });
 };
