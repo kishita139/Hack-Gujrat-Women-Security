34b2fa72b6ad02af5906cd580d8711569fc91ffc
xANARKYx <shikhars3145@gmail.com>
2020-09-06 07:56:41 +0530

    profile rendering as per user

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react'
-import {Avatar} from "antd";
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../contexts';
+import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import Header from "./Header";
-import "./profile.css";
-export default class Profile extends Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-                <div className="container1">
-               <div id="avatar"> <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size={70}/></div>
-               <div className="username"><h2>Kshatakshi Gupta</h2></div>
-               </div>
-               <ul>
-                   <li>Mob Number: 9685266</li>
-                   <li>Address: B-123 Rosewood</li>
-                   <li>Emergency Numbers:
-                       <ol><li>123456789</li>
-                   <li>875673452</li>
-                   <li>765846392</li>
-                   </ol>
-                   </li>
-                   
-               </ul>
-            </div>
-        )
-    }
+import Header from './Header';
+import './profile.css';
+export default function Profile() {
+  const { user, setUser } = useContext(UserContext);
+  return (
+    <div>
+      <Header />
+      <div className="container1">
+        <div id="avatar">
+          {' '}
+          <Avatar
+            style={{ backgroundColor: '#87d068' }}
+            icon={<UserOutlined />}
+            size={70}
+          />
+        </div>
+        <div className="username">
+          <h2>{user.name}</h2>
+        </div>
+      </div>
+      <ul>
+        <li>Mob Number: {user.phone_no}</li>
+        <li>
+          Emergency Numbers:
+          <ol>
+            {user.contact_info.map((contact) => {
+              if (contact)
+                return (
+                  <li>
+                    <input defaultValue={contact} />
+                  </li>
+                );
+            })}
+          </ol>
+        </li>
+      </ul>
+    </div>
+  );
 }
